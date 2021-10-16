3232700c4cf2e548e07f04ed11dbe65669eef0c4
Emanuele Lia <lia.emanuele13@gmail.com>
2021-10-17 01:57:32 +0200

    Added new searchfromlibrary.py file. This is the second tool for search name and provide grades.

---
+++ searchfromlibrary.py
@@ -0,0 +1,10 @@
+import json
+import time
+
+print('Research a name from the Json course and receive the grade.\nWrite one of the following command:')
+print('___________________________________________________________')
+print('||   -r   || Research a name from the library            ||')
+print('||   -n   || Show the Name List                          ||')
+print('||   -g   || Show the Grade List                         ||')
+print('||   -e   || Exit                                        ||')
+print('\n')
