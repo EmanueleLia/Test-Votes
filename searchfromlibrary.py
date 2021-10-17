af94615394e9a7136c8217de52c3a4f4d7c9f764
Emanuele Lia <lia.emanuele13@gmail.com>
2021-10-17 10:21:55 +0200

    Added the first simple research functionality: 1) Added the Import Json file functionality; 2) Put a Json file into a string; 3) The name entered as input is converted to upper(); 2) Used the Python Dictionary in order to check:     a) If a Key with the same value in input exists;     b) It shows the correspondant value.
    Otherwise it'll be shown "No name found".
    More details will be added soon.

--- searchfromlibrary.py
+++ searchfromlibrary.py
@@ -1,13 +1,32 @@
 import json
 import time
 
-print('Research a name from the Json course and receive the grade.\nWrite one of the following command:')
+print('Research a name from the Json course and receive the grade.\nSelect one of the following options:')
+
+time.sleep(0.5)
+
 print('___________________________________________________________')
-print('||   -r   || Research a name from the library            ||')
-print('||   -n   || Show the Name List                          ||')
-print('||   -g   || Show the Grade List                         ||')
-print('||   -e   || Exit                                        ||')
+print('||   1   ||  Research a name from the library            ||')
+print('||   2   ||  Show the Name List                          ||')
+print('||   3   ||  Show the Grade List                         ||')
+print('||   4   ||  Exit                                        ||')
+print('***********************************************************')
 print('\n')
 
+time.sleep(1)
+
 while True:
-    menuchoice = str(input('Enter one of the commands\n>> '))
+    menuchoice = int(input('Enter one of the commands\n>> '))
+    if menuchoice == 1:
+        #save the Json file in input into the data string
+        with open('namelist.json','r') as json_file:
+            data = json.load(json_file)
+        #request the name to be searched 
+        namecapital = input('\nEnter the name you want to search:\n>> ').upper()
+        #search namecapital in the json keys
+        if namecapital in data:
+            print(data[namecapital])
+        else:
+            print('Name not found')
+
+        
