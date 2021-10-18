bbbd44985b71d1798d86969ae21efe5a45d375a3
Emanuele Lia <lia.emanuele13@gmail.com>
2021-10-18 08:04:26 +0200

    Completed the research tool: 1) The tool now is able to show the researched name; 2) From the menu the user is able to see all the Json keys; 3) From the menu the user is able to see all the Json values; 4) From the menu the user is able to exit.

--- searchfromlibrary.py
+++ searchfromlibrary.py
@@ -1,32 +1,63 @@
 import json
 import time
 
+#save the Json file in input into the data string
+with open('namelist.json','r') as json_file:
+    data = json.load(json_file)
+
+#initiate the Command Line
 print('Research a name from the Json course and receive the grade.\nSelect one of the following options:')
-
 time.sleep(0.5)
-
 print('___________________________________________________________')
 print('||   1   ||  Research a name from the library            ||')
 print('||   2   ||  Show the Name List                          ||')
 print('||   3   ||  Show the Grade List                         ||')
 print('||   4   ||  Exit                                        ||')
 print('***********************************************************')
-print('\n')
-
 time.sleep(1)
 
 while True:
     menuchoice = int(input('Enter one of the commands\n>> '))
     if menuchoice == 1:
-        #save the Json file in input into the data string
-        with open('namelist.json','r') as json_file:
-            data = json.load(json_file)
         #request the name to be searched 
-        namecapital = input('\nEnter the name you want to search:\n>> ').upper()
+        namecapital = input('Enter the name you want to search:\n>> ').upper()
         #search namecapital in the json keys
         if namecapital in data:
-            print(data[namecapital])
+            print('\nGrade: '+str(data[namecapital]))
         else:
             print('Name not found')
+        #return to the main menu
+        time.sleep(1.5)
+        print('\nDo you want to continue?\nSelect one of the following options:')
+        print('___________________________________________________________')
+        print('||   1   ||  Research a name from the library            ||')
+        print('||   2   ||  Show the Name List                          ||')
+        print('||   3   ||  Show the Grade List                         ||')
+        print('||   4   ||  Exit                                        ||')
+        print('***********************************************************')
+    if menuchoice == 2:
+        print(str(data.keys())+'\n')
+        time.sleep(1.5)
+        print('\nDo you want to continue?\nSelect one of the following options:')
+        print('___________________________________________________________')
+        print('||   1   ||  Research a name from the library            ||')
+        print('||   2   ||  Show the Name List                          ||')
+        print('||   3   ||  Show the Grade List                         ||')
+        print('||   4   ||  Exit                                        ||')
+        print('***********************************************************')
+    if menuchoice == 3:
+        print(str(data.values())+'\n')
+        time.sleep(1.5)
+        print('\nDo you want to continue?\nSelect one of the following options:')
+        print('___________________________________________________________')
+        print('||   1   ||  Research a name from the library            ||')
+        print('||   2   ||  Show the Name List                          ||')
+        print('||   3   ||  Show the Grade List                         ||')
+        print('||   4   ||  Exit                                        ||')
+        print('***********************************************************')
+    if menuchoice == 4:
+        break
+
+print('\nHave a good day sir.\n')
 
         
